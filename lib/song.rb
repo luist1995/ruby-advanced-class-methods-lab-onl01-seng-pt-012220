4b720dee6fcbfca9907fc30e3cd8e0f84af95038
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-22 03:36:10 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,58 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    @@all << self.new
+  end
+  
+  def self.create
+    song = self.new 
+    song.save
+    song
+  end
+  
+  def self.new_by_name(song_name)
+    song = Song.create
+    song.name = song_name
+    song 
+  end
+    
+  def self.create_by_name(song_name)
+    self.new_by_name(song_name)
+  end
+  
+  def self.find_by_name(song_name)
+   #binding.pry
+   self.all.find {|song| song.name == song_name}
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    song = self.find_by_name(song_name)
+    if song 
+      song
+    else
+      self.create_by_name(song_name)
+    end
+  end 
+  
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+  
+  def self.new_from_filename(song)
+    split_file = song.split (" - ")
+    artist = split_file[0]
+    song_name = split_file[1].split(".")[0]
+    song = self.new_by_name(song_name)
+    song.artist_name = artist 
+    song
+  end
+  
+  def self.create_from_filename(song)
+    self.new_from_filename(song)
+  end
+  
+  def self.destroy_all 
+    self.all.clear
+  end
 end
